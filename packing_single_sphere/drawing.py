0e84ccfcf370f13daca3e963a781c4313b10c0b5
liusinuo <787666673@qq.com>
2020-05-01 23:53:59 -0400

    add comment

--- packing_single_sphere/drawing.py
+++ packing_single_sphere/drawing.py
@@ -3,10 +3,63 @@ import matplotlib.pyplot as plt
 import numpy as np
 import json
 
+def show_center_img(x,y,z):
+    print("Show distribution img of protein's center")
+    ax = plt.subplot(111, projection='3d')  # create project
+    ax.scatter(x, y, z, c='r')  # draw
+
+    ax.set_zlabel('Z')  # aix
+    ax.set_ylabel('Y')
+    ax.set_xlabel('X')
+    plt.show()
 
 
+def show_sum_img(sumlist, protein_num):
+    ##process sumlist
+    newlist = process_sumlist(sumlist, protein_num)
+    length = int(len(sumlist) / protein_num)
+    print(length)
+    print('Show sum img')
+    ax = plt.subplot()
+    for line in range(protein_num):
+        ax.plot(newlist[length * line:length * (line + 1)], label='protein' + str(line + 1))
+    plt.legend()
+    plt.title('Loss of each protein')
+    plt.xlabel('Iteration')
+    plt.ylabel('Loss')
 
-def drawing_function(radius_list,loc_list):
+    # print('Length of sumlist is:',len(sumlist))
+    plt.show()
+
+
+def process_sumlist(sumlist, protein_num):
+    newlist = [0 for _ in range(len(sumlist))]
+    length = int(len(sumlist) / protein_num)
+    print(length)
+    for ii in range(len(sumlist)):
+        temp_remainder = ii % protein_num
+        temp_quotient = int((ii - temp_remainder) / protein_num)
+        newlist[temp_remainder * length + temp_quotient] = sumlist[ii]
+    return newlist
+
+def get_packing_and_plot_ball(optimal_result,boundary_shpere):
+    pdb_id = optimal_result['pdb_id']
+    x_loc = optimal_result['x']
+    y_loc = optimal_result['y']
+    z_loc = optimal_result['z']
+
+
+    radius_list = []
+    center_list = [x_loc,y_loc,z_loc]
+    for ii in range(len(pdb_id)):
+        radius_list.extend([boundary_shpere[pdb_id[ii]]['radius'][0]])
+    print(radius_list)
+    print(center_list)
+
+    drawing_center_with_ball(radius_list,center_list)
+
+
+def drawing_center_with_ball(radius_list, loc_list):
 
 
     center = [loc_list[0][0],loc_list[1][0],loc_list[2][0]]
@@ -36,18 +89,14 @@ def drawing_function(radius_list,loc_list):
     plt.show()
 
 
+def get_json_and_plot_ball(file):
 
-
-
-def get_json_and_plot(path):
-
-    with open('packing.json') as f:
-     packing_result = json.load(f)
-     pdb_id = packing_result['optimal_result']['pdb_id']
-     x_loc = packing_result['optimal_result']['x']
-     y_loc = packing_result['optimal_result']['y']
-     z_loc = packing_result['optimal_result']['z']
-
+    with open(file) as f:
+        packing_result = json.load(f)
+        pdb_id = packing_result['optimal_result']['pdb_id']
+        x_loc = packing_result['optimal_result']['x']
+        y_loc = packing_result['optimal_result']['y']
+        z_loc = packing_result['optimal_result']['z']
 
     radius_list = []
     center_list = [x_loc,y_loc,z_loc]
@@ -55,12 +104,9 @@ def get_json_and_plot(path):
         radius_list.extend([packing_result['boundary_shpere'][pdb_id[ii]]['radius'][0]])
     print(radius_list)
     print(center_list)
+    drawing_center_with_ball(radius_list,center_list)
 
-    drawing_function(radius_list,center_list)
-
-
-
-######################### test part ####################
-path='packing.json'
-get_json_and_plot(path)
-    
+# ######################### test part ####################
+# file='../IOfile/packing.json'
+# get_json_and_plot_ball(file)
+#
